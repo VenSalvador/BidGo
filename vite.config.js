9c34f0c7015b21f746488f0765135f6eafeac24d
VenSalvador <raven.jssalvador@gmail.com>
2024-07-20 17:56:19 +0800

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': '/resources/js',
-            ziggy: path.resolve(__dirname, 'vendor/tightenco/ziggy/src/js/route.js'),
+            ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
         },
     },
     build: {
