7b7d7f449b075be45f2165e90df03a786a9f04c1
VenSalvador <raven.jssalvador@gmail.com>
2024-07-22 19:45:30 +0800

    Deploy

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://your-frontend-domain.com'],
+    'allowed_headers' => ['*'],
+    'supports_credentials' => true,
+];
